6e0a5379aa2e433620a3134b6f75d4269d44f965
Daniel Johnson <daniel.johnson@surescripts.com>
2015-05-22 15:47:35 -0500

    packaged solution for xml choice

---
+++ xmltogo/xml.go
@@ -0,0 +1,43 @@
+package xmltogo
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+type InterfaceMarshaller struct {
+	ChildMap map[string]reflect.Type
+}
+
+func (i *InterfaceMarshaller) MarshalChildren(d *xml.Decoder, start xml.StartElement, appender func(item interface{}) error) error {
+	for true {
+		t, err := d.Token()
+		if err != nil {
+			return err
+		}
+		if reflect.DeepEqual(t, start.End()) {
+			break
+		}
+		if reflect.DeepEqual(reflect.TypeOf(t), reflect.TypeOf(start)) {
+			bookend, ok := t.(xml.StartElement)
+			if ok {
+				itemType, ok := i.ChildMap[bookend.Name.Local]
+				if !ok {
+					return errors.New(fmt.Sprintf("Token name not recognized: %v", bookend.Name.Local))
+				}
+				item := reflect.New(itemType).Interface()
+				err := d.DecodeElement(&item, &bookend)
+				if err != nil {
+					return err
+				}
+				err = appender(item)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
---
+++ xmltogo/xml_test.go
@@ -0,0 +1,76 @@
+package xmltogo_test
+
+import (
+	"./"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"reflect"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	bytes, err := ioutil.ReadFile("./data.xml")
+	if err != nil {
+		panic(err)
+	}
+	a := A{}
+	err = xml.Unmarshal(bytes, &a)
+	if err != nil {
+		panic(err)
+	}
+	for _, item := range a.List {
+		fmt.Printf("item: %v\n", item.String())
+	}
+}
+
+var marshallerOfA = xmltogo.InterfaceMarshaller{
+	ChildMap: map[string]reflect.Type{
+		"b": reflect.TypeOf(B{}),
+		"c": reflect.TypeOf(C{}),
+		"d": reflect.TypeOf(D{}),
+	},
+}
+
+type A struct {
+	List []Inner
+}
+
+func (a *A) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	//var end *xml.EndElement
+	return marshallerOfA.MarshalChildren(d, start, func(item interface{}) error {
+		innerItem, ok := item.(Inner)
+		if ok {
+			a.List = append(a.List, innerItem)
+		}
+		return nil
+	})
+}
+
+type Inner interface {
+	String() string
+}
+
+type B struct {
+	Data string `xml:",chardata"`
+}
+
+func (b *B) String() string {
+	return "b:" + b.Data
+}
+
+type C struct {
+	Data string `xml:",chardata"`
+}
+
+func (c *C) String() string {
+	return "c:" + c.Data
+}
+
+type D struct {
+	Data string `xml:",chardata"`
+}
+
+func (d *D) String() string {
+	return "d:" + d.Data
+}
