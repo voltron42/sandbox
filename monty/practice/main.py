e2a00c4e6fe372fc0e70e90e1cbfe1838c3793fb
Daniel Johnson <daniel.johnson@surescripts.com>
2016-03-04 15:42:29 -0600

    adding python practice

---
+++ monty/practice/main.py
@@ -0,0 +1,3 @@
+import app.app as app
+
+app.main()
---
+++ monty/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+   name='Practice',
+   version='0.1dev',
+   packages=['practice',],
+   license='MIT',
+   long_description=open('README.txt').read(),
+)
