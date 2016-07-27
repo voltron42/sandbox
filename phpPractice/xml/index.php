1ea2fab38b7d2b3bf5a8eb63380b2c0e4085d12e
Daniel Johnson <daniel.johnson@surescripts.com>
2016-07-26 19:26:55 -0500

    demoing parser from php website using XMLReader

--- phpPractice/xml/index.php
+++ phpPractice/xml/index.php
@@ -7,12 +7,27 @@ interface Node {
 }
 
 
-$xml = '<document title="hi" from="there" to="steve"><body id="1">dave</body><head id="3">Monkeys</head><body id="2">thomas</body></document>';
+$xml = '<document title="hi" from="there" to="steve">'
+.'<body id="1">dave</body>'
+.'okay'
+.'<head id="3">Monkeys</head>'
+.'not okay'
+.'<body id="2">thomas</body>'
+.'</document>';
 
+echo "<PRE>";
+echo "parser 1: <br/>\n";
 $obj1 = XMLParser::parse($xml, "Note");
-$obj2 = XMLParser2::parse($xml);
-
 var_dump($obj1);
+echo "<br/>\n";
+echo "parser 2: <br/>\n";
+$obj2 = XMLParser2::parse($xml);
 var_dump($obj2);
+echo "<br/>\n";
+echo "parser 3: <br/>\n";
+$obj3 = XMLParser3::parse($xml);
+var_dump($obj3);
+echo "<br/>\n";
+echo "</PRE>";
 
 ?>
--- phpPractice/xml/xml.php
+++ phpPractice/xml/xml.php
@@ -16,4 +16,61 @@ class XMLParser2 {
   }
 }
 
+class XMLParser3 {
+  static function parse(string $raw) {
+    $xml = new XMLReader();
+    $xml->xml($raw);
+    $assoc = self::xml2assoc($xml, "root");
+    $xml->close();
+    return $assoc[0];
+  }
+
+  static function xml2assoc($xml, $name) {
+      print "<ul>\n";
+
+      $tree = null;
+      print("I'm inside " . $name . "<br>\n");
+
+      while($xml->read()) {
+          if($xml->nodeType == XMLReader::END_ELEMENT) {
+              print "</ul>\n";
+              return $tree;
+          } else if($xml->nodeType == XMLReader::ELEMENT) {
+              $node = array();
+
+              print("Adding " . $xml->name ."<br>\n");
+              $node['tag'] = $xml->name;
+
+              if($xml->hasAttributes) {
+                  $attributes = array();
+                  while($xml->moveToNextAttribute()) {
+                      print("Adding attr " . $xml->name ." = " . $xml->value . "<br>\n");
+                      $attributes[$xml->name] = $xml->value;
+                  }
+                  $node['attr'] = $attributes;
+              }
+
+              if(!$xml->isEmptyElement) {
+                  $childs = self::xml2assoc($xml, $node['tag']);
+                  $node['childs'] = $childs;
+              }
+
+              print($node['tag'] . " added <br>\n");
+              $tree[] = $node;
+          } else if($xml->nodeType == XMLReader::TEXT) {
+              $text = $xml->value;
+              $tree[] = $text;
+              print "text added = " . $text . "<br>\n";
+          }
+      }
+
+      print "returning " . count($tree) . " childs<br>\n";
+      print "</ul>\n";
+
+      return $tree;
+  }
+}
+
+
+
 ?>
