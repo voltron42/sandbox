84ca1c76a66f516a5dc5112a3f526f1fcc1defcb
Daniel Johnson <daniel.johnson@surescripts.com>
2017-07-11 06:25:23 -0500

    adding multi-page svg blocks

--- edn/src/documental/core.clj
+++ edn/src/documental/core.clj
@@ -14,21 +14,24 @@
 (defn -main [& [in out]]
   (try
     (pdf
-      [{:left-margin 18
-        :right-margin 18
-        :top-margin 36
-        :bottom-margin 36
-        :size :letter
-        :footer false}
-       [:svg {:under true}
-        (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE svg>"
-             (with-out-str
-               (xml/emit-element
-                 (x-pand
-                   (into [:svg {:xmlns "http://www.w3.org/2000/svg"
-                                :width 612
-                                :height 792}]
-                         (edn/read-string (slurp (make-file in))))))))]]
+      (into [{:left-margin 18
+              :right-margin 18
+              :top-margin 36
+              :bottom-margin 36
+              :size :letter
+              :footer false}]
+            (interpose [:pagebreak]
+                       (map (fn [block]
+                              [:svg {:under true}
+                               (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE svg>"
+                                    (with-out-str
+                                      (xml/emit-element
+                                        (x-pand
+                                          (into [:svg {:xmlns "http://www.w3.org/2000/svg"
+                                                       :width 612
+                                                       :height 792}]
+                                                block)))))])
+                            (edn/read-string (slurp (make-file in))))))
       out)
     (catch Exception e
       (println (.getMessage e))
